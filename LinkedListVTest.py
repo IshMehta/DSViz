003a0452540498d0ef489e1369b68376ee500ae2
Ish Mehta <ish.mehta2310@gmail.com>
2021-06-07 14:57:53 +0530

    Update Linked List example

--- LinkedListVTest.py
+++ LinkedListVTest.py
@@ -5,6 +5,6 @@ from DSViz.LinkedListV import LinkedListV
 test = LinkedListV(CLL=True, DLL=True)
 
 for i in range(97, 123):
-    test.list.append(chr(i))
-    print(chr(i))
+    test.addNode(chr(i))
+
 test.show
