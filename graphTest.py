5b6a8e1951e9c3b6c90659bdce67797cffea3088
IshMehta <ish.mehta2310@gmail.com>
2021-07-17 23:15:41 +0530

    test updates

--- graphTest.py
+++ graphTest.py
@@ -6,10 +6,10 @@ adjlist = {'0': ['1','4'],
            '2': ['1','3'],
            '3': ['1','2','4'],
            '4': ['0','1','3']}
-# test = graph(adjlist)
-# test.visuliase('0')
+test = graph(adjlist)
+test.visuliase('0')
 
-t = GraphV()
-for e in adjlist.keys():
-    t.add(e, adjlist[e])
-t.show
+# t = GraphV()
+# for e in adjlist.keys():
+#     t.add(e, adjlist[e])
+# t.show
