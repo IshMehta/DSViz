ef1bf594770bc10d2a5a0c75e7fbda1b6459665d
IshMehta <ish.mehta2310@gmail.com>
2021-07-26 17:36:05 +0530

    Cleanup

--- DSViz/TreeV.py
+++ DSViz/TreeV.py
@@ -3,22 +3,6 @@ from graphviz import Digraph
 class TreeV:
     def __init__(self, title = "Tree"):
         self.dot = Digraph(comment= title)
-    '''
-    delete this part
-
-    #     self.size = 0
-
-    # def addNode(self, parent, left, right):
-    #     if self.size == 0:
-    #         self.dot.node(parent, parent)
-    #     # else:
-    #         self.dot.node(left, left)
-    #         self.dot.node(right, right)
-    #         # self.dot.edges([[str(parent)+str(left), str(parent)+str(right)]])
-    #         self.dot.edge(parent, right)
-    #         self.dot.edge(parent, left)
-    #         self.dot.edge(left, "D")
-    '''
             
     def add(self, parent, left = None, right = None):
         self.dot.node(str(parent),str(parent))
@@ -36,5 +20,4 @@ class TreeV:
     @property
     def show(self):
         self.dot.render('DSViz/test-output/graph.gv', view=True)
-        print(self.dot.source)
 
