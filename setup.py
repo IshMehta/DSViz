09241e55d33564e2ccbf3d761f2ecdafe1cfc8eb
IshMehta <ish.mehta2310@gmail.com>
2021-08-02 13:11:49 +0530

    Setup.py version  update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   packages = ['DSViz'],   
   name = 'DataStructureViz',       
-  version = '0.0.1',      
+  version = '1.0.0',      
   license='MIT',        
   description = 'DSViz is a simple and intuitive Python interface to multiple packages in order to help visualise different data structres while coding them. This package is developed mainly for students or developers who are in the process of learning data structures.',   # Give a short description about your library
   author = 'Ish Mehta',                   
